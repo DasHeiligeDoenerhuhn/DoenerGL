bc53f58d5cd558592c4dacdb0d49e5c2b2adb938
DasHeiligeDoenerhuhn <93918690+DasHeiligeDoenerhuhn@users.noreply.github.com>
2021-12-13 14:01:29 +0100

    Update DönerGL.hpp

--- DönerGL.hpp
+++ DönerGL.hpp
@@ -10,3 +10,8 @@ int CamZPos = 100;
 	line(FoV*(x2+CamXPos)/(CamZPos+z2), FoV*(y2+CamYPos)/(z2+CamZPos),FoV*(x3+CamXPos)/(CamZPos+z3), FoV*(y3+CamYPos)/(z3+CamZPos),color(0,0,255));
 	line(FoV*(x3+CamXPos)/(CamZPos+z3), FoV*(y3+CamYPos)/(z3+CamZPos),FoV*(x+CamXPos)/(CamZPos+z), FoV*(y+CamYPos)/(z+CamZPos),color(0,0,255));	
 	}
+void DrawTri3D(int x, int y, int z, int x2, int y2, int z2, int x3, int y3,int z3, int xRot, int yRot, int zRot){
+
+//do stuff
+	
+}
